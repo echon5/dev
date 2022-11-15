aa108fdd8cb27e590bc124e71f9a082ceafd036c
Erin Chon <echon@bsd220.org>
2022-11-15 17:27:37 -0600

    Initial Commit

---
+++ FINAL PROJECT/FINAL_PROJECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  FINAL_PROJECTApp.swift
+//  FINAL PROJECT
+//
+//  Created by Erin Chon on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct FINAL_PROJECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
